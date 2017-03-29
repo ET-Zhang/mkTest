311d6651444224980b99dbf65e439fc4f865d82c
zhangyingjie <zhangyj@wisenjoy.com>
2017-03-29 18:25:03 +0800

    测试

---
+++ mkTest/react-webpack-express/src/js/components/Tab3.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Tab3 extends Component {
+
+  render() {
+    return (
+      <div>
+       	<p>
+       		"ceshi wenjian "
+       	</p>	
+      </div>
+    );
+  }
+
+}
+
+module.exports = Tab3;
